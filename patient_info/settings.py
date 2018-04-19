0aee35faaa3a0c842727ed95c8d8d7249d189aca
apoorv <apoorvkhare007@gmail.com>
2018-04-19 15:31:36 +0530

    changes in settings.py

--- patient_info/settings.py
+++ patient_info/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '#1+!10(=8o_23**gkd=u*(akfalh(nl37(*$rhn3mlr7fjpsiv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
